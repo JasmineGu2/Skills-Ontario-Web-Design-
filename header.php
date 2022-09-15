b4cb8cfc8f16e2cbd170c24a3f069dae73588668
JasmineGu2 <73567144+JasmineGu2@users.noreply.github.com>
2022-09-15 18:18:47 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -1,23 +1,14 @@
-<?php
-    if(isset($_POST['logout'])){
-        $_SESSION['loggedIn'] = FALSE;
-        session_destroy();
-        header("Location: ../admin/index.php");
-    }
- ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Header</title>
-    <!--bootstrap -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Header Template</title>
+    <script src="jquery-3.6.0.min.js"></script>
+     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <!--Jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script>
-    <!--Favico -->
+     <!--Favico -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon_package_v0.16/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon_package_v0.16/favicon-16x16.png">
@@ -26,15 +17,16 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <!--CSS -->
-    <link rel="stylesheet" href="../../assets/styles.css">
+    <link rel="stylesheet" href="assets/styles.css">
 
-</head>
-<body>
-<div class="container-fluid">
-  <nav class="navbar navbar-expand-sm navbar-white border border-light px-4">
-    <a href="homepage.php" class="navbar-brand"> 
-      <img src="../assets/images/logo.png" alt="Kelly's Collars Logo" width="250px">
-    </a>
+
+  </head>
+  <body>
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-sm navbar-white border border-light px-4">
+      <a href="" class="navbar-brand">
+          <img src="assets/images/logo.png" alt="Kelly's Collars Logo" width="250px">
+      </a>
       <button
         type="button"
         class="navbar-toggler"
@@ -48,19 +40,25 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarToggle">
         <ul class="navbar-nav ms-auto">
-          <li class="btn btn-light btn-outline-dark px-3 pt-3">
-            <a href="./?homepage" class="text-dark">HOMEPAGE</a>
+          <li class="btn btn-light btn-outline-dark px-3 mx-1">
+            <a href="index.php" class="nav-link text-dark">HOME</a>
           </li>
-          <li class="btn btn-light btn-outline-dark px-3 pt-3">
-            <a href="./?products" class="text-dark"> MANAGE PRODUCTS</a>
+          <li class="btn btn-light btn-outline-dark px-5 mx-1">
+            <a href="product.php" class="nav-link text-dark">SHOP</a>
           </li>
-          <li>
-              <form action="<?php $_SERVER['PHP_SELF'] ?> " method= "POST">
-                <input type="submit" value="logout" name="logout" class="btn btn-outline-dark py-3">      
-            </form>
+          <li class="btn btn-light btn-outline-dark px-3 mx-1">
+            <a href="cart.php" class="nav-link  text-dark">CART</a>
+          </li>
+          <li class="btn btn-light btn-outline-dark  mx-1" >
+            <a href="about.php" class="nav-link  text-dark">ABOUT</a>
+          </li>
+          <li class="btn btn-light btn-outline-dark mx-1">
+            <a href="contact.php" class="nav-link  text-dark">CONTACT</a>
           </li>
         </ul>
+        
       </div>
     </nav>
+  </div>
 </body>
-</html>
+    
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-  session_start();
-  
-  if(!$_SESSION['loggedIn']) {
-    include("loginform.php");
-  }
+
 
 ?>
 <!DOCTYPE html>
@@ -11,10 +7,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!--bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+      <!--bootstrap -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!--Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script>
     <!--Favico -->
@@ -25,25 +21,85 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
-
+    <!--CSS -->
+    <link rel="stylesheet" href="assets/styles.css">
+<style>
+    .blue {
+  background-color:#202155;
+  color: #202155;
+}
+</style>
 </head>
-<body>
-<?php
-      if ($_SESSION['loggedIn'] == TRUE) {
-        switch($_SERVER["QUERY_STRING"]) {
-          case "products":
-            include("productList.php");
-            break;
+    <div class="container blue h-150">
+        <p class="text-white py-2"> Call us at 647-987-1276!! </p>
+    </div>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+        <div class="col-3">
+        <navbar>
+            <ul class="navbar-nav ms-auto">
+            <li class="btn btn-white btn-outline-dark px-3 mx-1 py-4">
+                <a href="index.php" class="nav-link text-dark">HOME</a>
+            </li>
+             <li class="btn btn-white btn-outline-dark px-5 mx-1 py-4">
+                <a href="product.php" class="nav-link text-dark">SHOP</a>
+            </li>
+            <li class="btn btn-white btn-outline-dark px-3 mx-1 py-4">
+                <a href="cart.php" class="nav-link  text-dark">CART</a>
+            </li>
+            <li class="btn btn-white btn-outline-dark  mx-1 py-4" >
+                <a href="about.php" class="nav-link  text-dark">ABOUT</a>
+            </li>
+            <li class="btn btn-white btn-outline-dark mx-1 py-4">
+                <a href="contact.php" class="nav-link  text-dark">CONTACT</a>
+            </li>
+            </ul>
+        </navbar>
+        </div>
+        <div class="col-9">
+            <div class="container">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-text py-auto px-2"> 
+                            <img class= "textWrap" src="assets/images/logo.png" alt="Logo" width="500px">
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris metus metus, fringilla sit amet commodo ut, varius nec nisi. Cras in sagittis dui, et scelerisque nisl. Fusce at pellentesque erat. Integer luctus mauris augue, ut fringilla leo vehicula id. Fusce luctus nibh mi, vitae placerat neque luctus sit amet. Integer diam arcu, scelerisque vitae nunc eu, molestie faucibus nisi. Ut et diam ut ligula is natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc rhoncus consectetur auctor. Nullam volutpat, nulla et mattis ultricies, mi mauris pretium ante, eget condimentum augue leo sed odio. Suspendisse laoreet, dolor quis luctus elementum, lorem arcu placerat ante, in porta libero nunc at tortor.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="container-fluid py-3">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Who are we?</h4>
+            </div>
+            <div class="card-body">
+                <div class="card-text py-auto px-2">
+                    <img class= "textWrap" src="assets/images/dog1.jpg" alt="Image of dog with collar on" width="200px">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris metus metus, fringilla sit amet commodo ut, varius nec nisi. Cras in sagittis dui, et scelerisque nisl. Fusce at pellentesque erat. Integer luctus mauris augue, ut fringilla leo vehicula id. Fusce luctus nibh mi, vitae placerat neque luctus sit amet. Integer diam arcu, scelerisque vitae nunc eu, molestie faucibus nisi. Ut et diam ut ligula is natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc rhoncus consectetur auctor. Nullam volutpat, nulla et mattis ultricies, mi mauris pretium ante, eget condimentum augue leo sed odio. Suspendisse laoreet, dolor quis luctus elementum, lorem arcu placerat ante, in porta libero nunc at tortor.
+                </div>
+                
+            </div>
+            </div>
+        </div>
+<div class="container-fluid py-3">
+    <div class="row mt-5 justify-content-center">
+        <div class="col-12">
+            <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Our Mission? Happy Dogs!</h4>
+            </div>
+        </div>
+    </div>
+</div>
+<img src="assets/images/dog2.jpg" alt="Image of dog with collar on" width="400px">
+<img src="assets/images/dog4.jpg" alt="Image of dog with collar on" width="400px">
 
-          case "homepage":
-            include("homepage.php");
-            break;
 
-          default:
-            include("homepage.php");
-            break;
-        }
-      }
-    ?>
-</body>
+<?php include('footer.php'); ?>
+
 </html>
